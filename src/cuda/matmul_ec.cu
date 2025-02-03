b6a20597abf4b6d71bd0584fc71e5aed68298cd5
Francesco Risso <francesco@frisso.net>
2025-02-03 10:29:39 +0100

    Split chooser

--- src/cuda/matmul_ec.cu
+++ src/cuda/matmul_ec.cu
@@ -65,6 +65,42 @@ void print_CUDA_matrix(float* mat, int rows, int cols, const char* name, int fla
     CUDA_CHECK
 }
 
+/**
+ * @brief Decideds if the matrices should be split in blocks to fit the global memory, and if so how many
+ *
+ * @param[in]   rows_A                #rows of matrix A
+ * @param[in]   cols_A                #cols of matrix A
+ * @param[in]   cols_B                #cols of matrix B
+ * @param[out]  num_split_common_dim  The number of blocks for dividing A's columns and B's rows into (= the dimensions multiplied together)
+ * @param[out]  num_split_other_dim   The number of blocks for dividing A's rows and B's columns into (= the "other" dimensions)
+ *
+ */
+void choose_division(int rows_A, int cols_A, int cols_B, int* num_split_common_dim, int* num_split_other_dim)
+{
+    float required_mem = (rows_A + 1) * cols_A;  // A with checksum
+    required_mem += ROWS_B * (cols_B + 1);       // B with checksum
+    required_mem += (ROWS_C + 1) * (COLS_C + 1); // C with checksum
+    required_mem += COLS_C + 1;                  // Column checksum buffer for C
+    required_mem += ROWS_C + 1;                  // Row checksum buffer for C
+    required_mem *= sizeof(float);
+
+    required_mem += 2 * EDC_MAX_ERRORS * sizeof(int); // Mismatches index buffer for error correction
+    required_mem += 4 * sizeof(int);                  // Mismatch_count_x/y, error_x/y
+
+    float exceeding_factor = required_mem / globals::maxGlobalMem;
+
+    if (exceeding_factor <= 3)
+    {
+        *num_split_common_dim = ceil(exceeding_factor);
+        *num_split_other_dim = 1;
+    }
+    else
+    {
+        *num_split_common_dim = ceil(sqrt(exceeding_factor));
+        *num_split_other_dim = ceil(exceeding_factor / (float)(*num_split_common_dim));
+    }
+}
+
 namespace cuda
 {
     EDCResult matmul_ec(float* A, float* B, float* C, int rows_A, int cols_A, int cols_B, int errors_count, int* error_xs, int* error_ys, float* error_values)
