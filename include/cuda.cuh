e73b79ba6bc471b3940aa4317a6b86b23be8d397
Francesco Risso <francesco@frisso.net>
2025-02-06 16:37:57 +0100

    Rm useless files

--- include/cuda.cuh
+++ include/cuda.cuh
@@ -23,20 +23,6 @@ namespace cuda
     Info getInfo();
 
     /**
-     * @brief Computes the product between two matrices A and B, exploiting the GPU
-     * with the tiled multiplication algorithm
-     *
-     * @param[in]   A       The first matrix to mutiply
-     * @param[in]   B       The second matrix to multiply
-     * @param[out]  C       The result matrix
-     * @param[in]   rows_A  #rows in A
-     * @param[in]   cols_A  #cols in A
-     * @param[in]   cols_B  #cols in B
-     *
-     */
-    void matmul(float* A, float* B, float* C, int rows_A, int cols_A, int cols_B);
-
-    /**
      * @brief Computes the error corrected tiled matrix multiplication between A and B
      *
      * @param[in]   A             First operand matrix
