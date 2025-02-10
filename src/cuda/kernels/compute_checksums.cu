0b5a04672390b6b42c163a02ec0e5444b4f1399b
Emanuele <emanuelemessina.em@gmail.com>
2025-02-10 12:21:34 +0100

    transfers + ops comments

--- src/cuda/kernels/compute_checksums.cu
+++ src/cuda/kernels/compute_checksums.cu
@@ -14,21 +14,21 @@ __global__ void kernels::compute_checksums(float* matrix, int rows, int cols, Re
     int limit_reduction = reduction_direction == ReductionDirection::ALONG_COL ? rows : cols;
 
     // this thread accumulates values in blockdim offsets along the reduction direction
-    for (int i = index_reduction; i < limit_reduction; i += blockDim_reduction)
+    for (int i = index_reduction; i < limit_reduction; i += blockDim_reduction) // 1 op * N/blockdim
     {
-        sum += reduction_direction == ReductionDirection::ALONG_COL ? matrix[i * cols + index_orthogonal] : matrix[index_orthogonal * (cols + 1) + i];
+        sum += reduction_direction == ReductionDirection::ALONG_COL ? matrix[i * cols + index_orthogonal] : matrix[index_orthogonal * (cols + 1) + i]; // 1 transf, 3 ops | 4 ops (for each matmul, one of each type)
     }
 
     // this thread stores his partial result into shared memory at his relative offset
-    shared_data[index_reduction] = sum;
-    __syncthreads(); // other threads do the same for their relative offset, we wait for all threads to finish
+    shared_data[index_reduction] = sum; // 1 transf
+    __syncthreads();                    // other threads do the same for their relative offset, we wait for all threads to finish
 
     // this threads performs reduction over the shared memory vector
-    for (int stride = blockDim_reduction / 2; stride > 0; stride /= 2)
+    for (int stride = blockDim_reduction / 2; stride > 0; stride /= 2) // 1 op one time, log2(blockdim) iterations
     {
-        if (index_reduction < stride && index_reduction + stride < limit_reduction)
+        if (index_reduction < stride && index_reduction + stride < limit_reduction) // 1 op
         {
-            shared_data[index_reduction] += shared_data[index_reduction + stride];
+            shared_data[index_reduction] += shared_data[index_reduction + stride]; // 2 ops, 2 transf
         }
         __syncthreads(); // other threads do the same
     }
@@ -38,17 +38,20 @@ __global__ void kernels::compute_checksums(float* matrix, int rows, int cols, Re
     {
         if (checksum != nullptr)
         {
-            checksum[index_orthogonal] = shared_data[0];
+            checksum[index_orthogonal] = shared_data[0]; // in the control checksum case: 2 transf
             return;
-        }
+        } // in the load input checksums case
 
         if (reduction_direction == ReductionDirection::ALONG_COL)
         {
-            matrix[rows * cols + index_orthogonal] = shared_data[0]; // last row for column checksums
+            matrix[rows * cols + index_orthogonal] = shared_data[0]; // last row for column checksums // 2 transf, 2 ops
         }
         else
         {
-            matrix[index_orthogonal * (cols + 1) + cols] = shared_data[0]; // last column for row checksums
+            matrix[index_orthogonal * (cols + 1) + cols] = shared_data[0]; // last column for row checksums // 2 transf, 3 ops
         }
     }
 }
+
+// transfers: N*(N/blockdim)*1 + N*1 + N*log2(blockdim)*2 + 2
+// ops: N*(N/blockdim)*1 + N*(N/blockdim)*[N*3 (col) | N*4 (row)] + N*1 + N*log2(blockdim)*(1+1+2) + 1*[0 (control) | 2 (col) | 3 (row)]
--- src/cuda/kernels/find_checksum_mismatches.cu
+++ src/cuda/kernels/find_checksum_mismatches.cu
@@ -6,35 +6,35 @@
 
 __global__ void kernels::find_checksum_mismatches(const float* ec_matrix, int rows, int cols, float* control_checksum, ChecksumsToCompare checksums_to_compare, int* mismatch_count, int* mismatch_indexes, int* error_flag)
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int limit = checksums_to_compare == ChecksumsToCompare::ROW ? rows + 1 : cols + 1; // also verify checksums themselves
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;                                   // 2 op
+    int limit = checksums_to_compare == ChecksumsToCompare::ROW ? rows + 1 : cols + 1; // also verify checksums themselves  // 1 op
 
-    if (idx >= limit || *error_flag)
+    if (idx >= limit || *error_flag) // 1 transf
         return;
 
-    float mat_checksum_item = checksums_to_compare == ChecksumsToCompare::ROW ? ec_matrix[idx * (cols + 1) + cols] : ec_matrix[rows * (cols + 1) + idx];
-    float control_checksum_item = control_checksum[idx];
-    float diff = abs(mat_checksum_item - control_checksum_item);
+    float mat_checksum_item = checksums_to_compare == ChecksumsToCompare::ROW ? ec_matrix[idx * (cols + 1) + cols] : ec_matrix[rows * (cols + 1) + idx]; // 1 transf, 3 ops
+    float control_checksum_item = control_checksum[idx];                                                                                                 // 1 transfer
+    float diff = abs(mat_checksum_item - control_checksum_item);                                                                                         // 1 op
 
 #if CUDA_DEBUG_PRINT
     printf("%s thread %d: mat %f vs ctrl %f -> diff %f\n", checksums_to_compare == ChecksumsToCompare::ROW ? "row" : "col", idx, mat_checksum_item, control_checksum_item, diff);
 #endif
 
-    if (diff > 0.0001 * mat_checksum_item)
+    if (diff > 0.0001 * mat_checksum_item) // suppose 1 error per kernel, 1 thread in N triggers this // 1 op
     {
         // atomically update the mismatch count
-        int mismatch_idx = atomicAdd(mismatch_count, 1);
+        int mismatch_idx = atomicAdd(mismatch_count, 1); // 1 transfer, 1 op
 
 #if CUDA_DEBUG_PRINT
         printf("%s thread %d: current errors idx %d\n", checksums_to_compare == ChecksumsToCompare::ROW ? "row" : "col", idx, mismatch_idx);
 #endif
 
         // Check if the mismatch array is full
-        if (mismatch_idx >= EDC_MAX_ERRORS)
+        if (mismatch_idx >= EDC_MAX_ERRORS) // suppose this is not triggered
         {
             // mismatch array full: too many errors
             // set error flag and stop further processing
-            *error_flag = 1;
+            *error_flag = 1; // 1 transfer
 
 #if CUDA_DEBUG_PRINT
             printf("%s thread %d: too many errors, raising error flag\n", checksums_to_compare == ChecksumsToCompare::ROW ? "row" : "col", idx + 1);
@@ -43,6 +43,11 @@ __global__ void kernels::find_checksum_mismatches(const float* ec_matrix, int ro
             return;
         }
 
-        mismatch_indexes[mismatch_idx] = idx;
+        mismatch_indexes[mismatch_idx] = idx; // 1 transfer
     }
 }
+
+// transfers = (N-1)*3 + 1*2
+// transfer size = total transfers * sizeof(float)
+// ops = (N-1)*8 + 1*1
+// intensity [flops/byte] = ops / transfer size
